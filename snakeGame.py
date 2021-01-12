27ba0a7c4abe416d06bf823e9d09a5c280a35643
Tawishi <tawishisharma1@gmail.com>
2021-01-12 21:27:43 +0530

    first commit

---
+++ snakeGame.py
@@ -0,0 +1,66 @@
+import turtle
+import time
+
+delay = 0.1
+
+#set up the screen
+window = turtle.Screen()
+window.title("Snake Game")
+window.bgcolor("green")
+window.setup(width=600, height=600)
+window.tracer(0) #turns off animation on the screen
+
+# snake-head
+head = turtle.Turtle()
+head.speed(0) #animation speed of turtle module
+head.shape("square")
+head.color("black") 
+head.penup()  #turtle module by default draws a line
+head.goto(0,0)  #tutle starts at the centr of screen by default
+head.direction = "stop"
+
+# Function for moving of head
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_left():
+    head.direction = "left"
+
+def go_right():
+    head.direction = "right"
+
+
+
+def move():
+
+    y = head.ycor()
+    x = head.xcor()
+
+
+    if head.direction == "up":
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        head.sety(y - 20)
+    
+    if head.direction == "left":
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        head.setx(x + 20)
+    
+
+#main game loop
+while True:
+    window.update()
+
+    move()
+
+    # add delay
+    time.sleep(delay)
+
+
+window.mainloop()
